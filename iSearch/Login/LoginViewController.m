6d423fb1b9848c48acdb4bb38be0f821a8b9a280
jay16 <jay_li@intfocus.com>
2015-07-05 17:20:26 +0800

    fix#webview stretch width fully when open third part login.

--- iSearch/Login/LoginViewController.m
+++ iSearch/Login/LoginViewController.m
@@ -171,9 +171,9 @@
 - (IBAction)actionSubmit:(id)sender {
     self.labelPropmt.text = @"";
     
-    self.cookieValue = @"E99658602";
-    [self performSelector:@selector(actionOutsideLoginSuccessfully) withObject:self];
-    return;
+//    self.cookieValue = @"E99658602";
+//    [self performSelector:@selector(actionOutsideLoginSuccessfully) withObject:self];
+//    return;
     
     BOOL isNetworkAvailable = [HttpUtils isNetworkAvailable];
     NSLog(@"network is available: %@", isNetworkAvailable ? @"true" : @"false");
