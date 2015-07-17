31c69d66bdce88a0091f0b001ad5ab79ac3bc008
jay16 <jay_li@intfocus.com>
2015-07-17 15:21:38 +0800

    XCTest 阿拉伯数字转化为汉语数字

--- iSearchTests/Scripts/JayPlayTest.m
+++ iSearchTests/Scripts/JayPlayTest.m
@@ -21,11 +21,58 @@
 - (void)tearDown {
     [super tearDown];
 }
+#pragma mark -  阿拉伯数字转化为汉语数字
+- (NSString *)translation:(NSString *)arebic {
+    NSString *str             = arebic;
+    NSArray *arabic_numerals  = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"0"];
+    NSArray *chinese_numerals = @[@"一",@"二",@"三",@"四",@"五",@"六",@"七",@"八",@"九",@"零"];
+    NSArray *digits           = @[@"个",@"十",@"百",@"千",@"万",@"十",@"百",@"千",@"亿",@"十",@"百",@"千",@"兆"];
+    NSDictionary *dictionary  = [NSDictionary dictionaryWithObjects:chinese_numerals forKeys:arabic_numerals];
+    
+    NSMutableArray *sums = [NSMutableArray array];
+    for (int i = 0; i < str.length; i ++) {
+        NSString *substr = [str substringWithRange:NSMakeRange(i, 1)];
+        NSString *a = [dictionary objectForKey:substr];
+        NSString *b = digits[str.length -i-1];
+        NSString *sum = [a stringByAppendingString:b];
+        if ([a isEqualToString:chinese_numerals[9]])
+        {
+            if([b isEqualToString:digits[4]] || [b isEqualToString:digits[8]])
+            {
+                sum = b;
+                if ([[sums lastObject] isEqualToString:chinese_numerals[9]])
+                {
+                    [sums removeLastObject];
+                }
+            }else
+            {
+                sum = chinese_numerals[9];
+            }
+            
+            if ([[sums lastObject] isEqualToString:sum])
+            {
+                continue;
+            }
+        }
+        
+        [sums addObject:sum];
+    }
+    
+    NSString *sumStr = [sums  componentsJoinedByString:@""];
+    NSString *chinese = [sumStr substringToIndex:sumStr.length-1];
+    return chinese;
+}
+
+- (void)testTranslation {
+    XCTAssertEqualObjects([self translation:@"127"], @"一百二十七");
+    XCTAssertEqualObjects([self translation:@"1270"], @"一千二百七十");
+    XCTAssertEqualObjects([self translation:@"12700"], @"一万二千七百");
+}
+
 
 #pragma mark - args
 //1.参数类型是NSString类型,后面params是第一个参数,它后面跟着逗号和三个点(固定格式)
--(void)dynaArgs:(NSString*)params,...
-{
+- (void)dynaArgs:(NSString*)params,... {
     NSString* curStr;
     va_list list;
     if(params)
